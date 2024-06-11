13606e138fe21d480d5e93a366e8d6496e6f9228
FrankPujo <puginifrancesco@gmail.com>
2024-06-11 13:16:32 +0200

    V0.1

---
+++ res/script.js
@@ -0,0 +1,12 @@
+var gotoPiano = document.getElementById("pianoEl");
+gotoPiano.onclick = function() {
+	window.location.href = "./piano.html";
+};
+var gotoPosti = document.getElementById("postiEl");
+gotoPosti.onclick = function() {
+	window.location.href = "./posti.html";
+};
+var gotoCena = document.getElementById("cenaEl");
+gotoCena.onclick = function() {
+	window.location.href = "./cena.html";
+};
