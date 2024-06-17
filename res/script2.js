6fd7f225ea322e4234614448ab945e082809bf33
FrankPujo <puginifrancesco@gmail.com>
2024-06-17 19:34:20 +0200

    v2

---
+++ res/script2.js
@@ -0,0 +1,85 @@
+var m1 = document.getElementById("m1");
+m1.onclick = function() {
+	window.location.href = "https://stieglbrau.at/en/menu/";
+}
+var p1 = document.getElementById("p1");
+p1.onclick = function() {
+	window.location.href = "https://stieglbrau.at/en/reservation/";
+}
+
+//
+
+var m2 = document.getElementById("m2");
+m2.onclick = function() {
+	window.location.href = "https://app.winepad.at/de/public/goldener-adler-speisen";
+}
+var p2 = document.getElementById("p2");
+p2.onclick = function() {
+	window.location.href = "https://www.tripadvisor.at/Restaurant_Review-g190445-d3405638-Reviews-Goldener_Adler_Restaurant-Innsbruck_Tirol_Austrian_Alps.html";
+}
+
+//
+
+var m3 = document.getElementById("m3");
+m3.onclick = function() {
+	window.location.href = "https://www.diewilderin.at/";
+}
+var p3 = document.getElementById("p3");
+p3.onclick = function() {
+	window.location.href = "https://www.diewilderin.at/";
+}
+
+//
+
+var p4 = document.getElementById("p4");
+p4.onclick = function() {
+	window.location.href = "https://seegrube.at/en/";
+}
+var m4 = document.getElementById("m4");
+m4.onclick = function() {
+	window.location.href = "https://seegrube.at/en/";
+}
+
+//
+
+var m5 = document.getElementById("m5");
+m5.onclick = function() {
+	window.location.href = "https://www.fischerhaeusl.com/menus";
+}
+var p5 = document.getElementById("p5");
+p5.onclick = function() {
+	window.location.href = "https://www.tripadvisor.com/Restaurant_Review-g190445-d1390834-Reviews-Fischerhausl-Innsbruck_Tirol_Austrian_Alps.html";
+}
+
+//
+
+var m6 = document.getElementById("m6");
+m6.onclick = function() {
+	window.location.href = "https://ottoburg.at/#menu";
+}
+var p6 = document.getElementById("p6");
+p6.onclick = function() {
+	window.location.href = "https://ottoburg.at/kontakt/";
+}
+
+//
+
+var m7 = document.getElementById("m7");
+m7.onclick = function() {
+	window.location.href = "https://www.stiftskeller.eu/fileadmin/user_upload/dokumente/Speisekarte_GESAMT_2024.pdf";
+}
+var p7 = document.getElementById("p7");
+p7.onclick = function() {
+	window.location.href = "https://www.stiftskeller.eu/it/contatto";
+}
+
+//
+
+var m8 = document.getElementById("m8");
+m8.onclick = function() {
+	window.location.href = "https://zumwohltirol.at/?lang=it#impressions";
+}
+var p8 = document.getElementById("p8");
+p8.onclick = function() {
+	window.location.href = "https://zumwohltirol.at/?lang=it#reservation";
+}
